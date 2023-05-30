55908a3d127cc329adc18cd6e8166d584d02da1d
salmaciid <salmaciid44@gmail.com>
2023-05-30 16:36:55 +0300

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,51 @@
+import airbnb from "./airbnb.png";
+import bill from "./bill.png";
+import binance from "./binance.png";
+import card from "./card.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import robott from "./robott.png";
+import send from "./Send.png";
+import shield from "./Shield.svg";
+import star from "./Star.png";
+import menuu from "./menuu.png";
+import closee from "./closee.png";
+import apple from "./apple.png";
+import arrow from "./arrow.png"
+import facebook from "./facebook.png";
+import instagram from "./instagram.png";
+import linkedin from "./linkedin.png";
+import twitter from "./twitter.png";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+import quotes from "./quotes.png";
+import discount from "./discount.png"
+import google from "./google.png"
+
+export {
+  airbnb,
+  google,
+  bill,
+  binance,
+  card,
+  coinbase,
+  dropbox,
+  quotes,
+  robott,
+  send,
+  shield,
+  star,
+  menuu,
+  closee,
+  apple,
+  arrow,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+};
